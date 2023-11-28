8cf7f8488eae37e2b39f51f1370043d6b8c5b28e
kurganova <oekurgan@gmail.com>
2023-11-28 19:43:02 +0300

    decision Zadacha01

--- Zadacha01/Program.cs
+++ Zadacha01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine("да, кратно одновременно 7 и 23");
+else
+    Console.WriteLine("нет");
