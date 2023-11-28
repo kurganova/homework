7307372b611f34e8ff23c944a1c5cb6e141c3ffc
kurganova <oekurgan@gmail.com>
2023-11-28 19:48:49 +0300

    decision Zadacha03

--- Zadacha03/Program.cs
+++ Zadacha03/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+Console.Write("Введите число от 10 до 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n1; 
+int n2;
+n1 = number / 10;
+n2 = number % 10;
+if (n1 > n2)
+    Console.Write($"{n1}");
+else
+    Console.Write($"{n2}");
