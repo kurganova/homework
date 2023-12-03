9dd837db48b1a8e36c6f80b91906fc9a411b1fc8
kurganova <oekurgan@gmail.com>
2023-12-03 18:08:23 +0300

    add decision Zadacha02

--- Homework_Arrays/Zadacha02/Program.cs
+++ Homework_Arrays/Zadacha02/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: 
+// Задайте массив на 10 целых чисел. Напишите программу, которая определяет кол-во чётных чисел в массиве.
+
+void inputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 101); //[-100, 100]
+}
+
+int findCountOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    foreach (int element in array) 
+    {
+         if(element % 2 == 0)
+       count++;
+    }
+    return count;
+}
+
+Console.Clear();
+int n = 10;
+int[] array = new int[n];
+inputArray(array); 
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Кол-во четных чисел в массиве: {findCountOfEvenNumbers(array)}");
