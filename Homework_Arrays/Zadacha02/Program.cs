9e5c1cadd6f3c61198cbf6229ff7caf3ea68261d
kurganova <oekurgan@gmail.com>
2023-12-05 13:40:17 +0300

    correct decision Zadacha02

--- Homework_Arrays/Zadacha02/Program.cs
+++ Homework_Arrays/Zadacha02/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 2: 
 // Задайте массив на 10 целых чисел. Напишите программу, которая определяет кол-во чётных чисел в массиве.
 
-void inputArray(int[] array)
+void InputArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(-100, 101); //[-100, 100]
 }
 
-int findCountOfEvenNumbers(int[] array)
+int FindCountOfEvenNumbers(int[] array)
 {
     int count = 0;
     foreach (int element in array) 
@@ -21,6 +21,6 @@ int findCountOfEvenNumbers(int[] array)
 Console.Clear();
 int n = 10;
 int[] array = new int[n];
-inputArray(array); 
+InputArray(array); 
 Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Кол-во четных чисел в массиве: {findCountOfEvenNumbers(array)}");
+Console.WriteLine($"Кол-во четных чисел в массиве: {FindCountOfEvenNumbers(array)}");
