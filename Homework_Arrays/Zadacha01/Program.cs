774ac40607ae6fa764b3bfacfdb4dc5ec9e825cb
kurganova <oekurgan@gmail.com>
2023-12-05 13:40:01 +0300

    correct decision Zadacha01

--- Homework_Arrays/Zadacha01/Program.cs
+++ Homework_Arrays/Zadacha01/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов 
 // массива, значения которых лежат в отрезке [20,90].
 
-void inputArray(int[] array)
+void InputArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(1, 101); //[1, 100]
 }
 
-int isCheck(int[] array)
+int IsCheck(int[] array)
 {
     int count = 0;
     foreach (int element in array) 
@@ -21,6 +21,6 @@ int isCheck(int[] array)
 Console.Clear();
 int n = 10;
 int[] array = new int[n];
-inputArray(array); 
+InputArray(array); 
 Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Кол-во элементов массива, значения которых лежат в отрезке [20,90]: {isCheck(array)}");
+Console.WriteLine($"Кол-во элементов массива, значения которых лежат в отрезке [20,90]: {IsCheck(array)}");
