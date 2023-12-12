586493177723ac3f563147ca5d7e032f3eea26e0
kurganova <oekurgan@gmail.com>
2023-12-12 16:58:53 +0300

    add decision Zadacha03

--- Homework_Two-dimensional Arrays/Zadacha03/Program.cs
+++ Homework_Two-dimensional Arrays/Zadacha03/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }  
+}
+
+int SumRow(int[,] matrix, int i)
+{
+    int sumRow = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        sumRow += matrix[i, j];
+    }
+    return sumRow;
+}
+
+Console.Clear();
+int[,] mat = new int[4, 7];
+InputMatrix(mat);
+Console.WriteLine("Массив: ");
+PrintMatrix(mat);
+
+int minSumRow = 0;
+int sumRow = SumRow(mat, 0);
+for (int i = 1; i < mat.GetLength(0); i++)
+{
+    int temp = SumRow (mat, i);
+    if (sumRow > temp)
+    {
+        sumRow = temp;
+        minSumRow = i;
+    }
+}
+minSumRow = minSumRow + 1;
+
+Console.WriteLine();
+Console.WriteLine("Строка c наименьшей суммой элементов: " + minSumRow);
+Console.WriteLine("Сумма элементов: " + sumRow);
+
