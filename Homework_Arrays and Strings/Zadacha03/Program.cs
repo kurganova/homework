910f6a4c15c4cbad854d4412ca7e9d57fa23fe96
kurganova <oekurgan@gmail.com>
2023-12-12 18:04:14 +0300

    add decision Zadacha03

--- Homework_Arrays and Strings/Zadacha03/Program.cs
+++ Homework_Arrays and Strings/Zadacha03/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+bool IsPalindrome(string x)
+{   // Нормализация строки путем удаления не буквенно-цифровых символов и приведения к нижнему регистру 
+    string normalized = new string(x.Where(char.IsLetterOrDigit).ToArray()).ToLower(); 
+    // Сравнение строки с ее перевернутым вариантом 
+    return normalized.SequenceEqual(normalized.Reverse()); 
+}
+
+string input = "sTYTs";
+bool isPalindrome = IsPalindrome(input); 
+Console.Clear(); 
+Console.WriteLine(isPalindrome? "Да" : "Нет");
