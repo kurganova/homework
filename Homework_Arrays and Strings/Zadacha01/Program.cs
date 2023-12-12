c5430c85eaa79eb1c96f3ad98c7926dbd362e645
kurganova <oekurgan@gmail.com>
2023-12-12 17:48:53 +0300

    add decision Zadacha01

--- Homework_Arrays and Strings/Zadacha01/Program.cs
+++ Homework_Arrays and Strings/Zadacha01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+string CreateStringFromMatrix(char[,]matrix)
+{ string result = "";
+for (int i = 0; i < matrix.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {  
+            result += matrix[i, j]; 
+        } 
+    } 
+    return result; 
+}
+
+Console.Clear();
+char[,] charMatrix = new char[,] {{'a', 'b', '4'}, {'c', 'd', '2'}};
+string result = CreateStringFromMatrix(charMatrix);
+Console.WriteLine(result);
