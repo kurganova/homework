5fd41cebf9855e3adec22a95f10e61ed162f68b3
kurganova <oekurgan@gmail.com>
2023-12-12 18:14:22 +0300

    add decision Zadacha04

--- Homework_Arrays and Strings/Zadacha04/Program.cs
+++ Homework_Arrays and Strings/Zadacha04/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: 
+// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string ReverseWords(string x)
+{
+    // Разделение строки на слова 
+    string[] words = x.Split(' '); 
+    // Обращение порядка слов 
+    Array.Reverse(words); 
+    // Соединение слов обратно в строку спробелами 
+    return string.Join(" ", words);
+}
+
+string input = "I live in Moscow"; 
+string result = ReverseWords(input); 
+Console.Clear();
+Console.WriteLine(result);
