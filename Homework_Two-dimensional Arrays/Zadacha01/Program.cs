b0e4d9ac7a2044ad0a2ca9199a8517c92fab187b
kurganova <oekurgan@gmail.com>
2023-12-12 15:50:55 +0300

    add decision Zadacha01

--- Homework_Two-dimensional Arrays/Zadacha01/Program.cs
+++ Homework_Two-dimensional Arrays/Zadacha01/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }  
+}
+
+Console.Clear();
+Console.Write("Введите строку: ");
+int row = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int col = Convert.ToInt32(Console.ReadLine()) - 1;
+int[,] matrix = new int[6, 5];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+
+if (row < 0 | row > matrix.GetLength(0) - 1 | col < 0 | col > matrix.GetLength(1) - 1)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", matrix[row, col]);
+}
+
