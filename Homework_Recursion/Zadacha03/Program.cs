f2c6f3522958c5bef5beb12a5e651bbc4e2c60c3
kurganova <oekurgan@gmail.com>
2023-12-13 20:27:01 +0300

    add decision Zadacha03

---
+++ Homework_Recursion/Zadacha03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: 
+// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void ReverseArray(int[] array, int first, int last)
+{
+    if (first < last)
+    {
+        int temp = array[first];
+        array[first] = array[last];
+        array[last] = temp;
+        ReverseArray(array, first + 1, last - 1);
+    }
+}
+
+Console.Clear();
+int[] arr = {2, 5, 7, 1, 6, 31, 40, 99};
+ReverseArray(arr, 0, arr.Length - 1);
+string elements = String.Join(" ", arr);
+Console.WriteLine(elements);
