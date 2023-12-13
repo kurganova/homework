06d91806b5b911fbe933e2d7556240a5e7145d19
kurganova <oekurgan@gmail.com>
2023-12-13 20:58:40 +0300

    add decision Zadacha02

--- Homework_Recursion/Zadacha02/Program.cs
+++ Homework_Recursion/Zadacha02/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if ((m > 0) && (n == 0))
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+Console.Clear();
+int m = 2;
+int n = 3;
+Console.WriteLine($"Функция Аккермана A(m, n) = {Akkerman(m, n)}");
